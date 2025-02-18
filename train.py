1ccc872269df1c476e8579a5510e33243806f137
Parker <pushkars188@gmail.com>
2025-02-18 17:44:34 +0530

    Update train.py
    Added comments

--- train.py
+++ train.py
@@ -1,31 +1,38 @@
+# Importing modules
 from anomalib.data import Folder
 from anomalib.models import Patchcore, Padim
 from anomalib.engine import Engine
 from anomalib import TaskType
 from anomalib.deploy import ExportType
 
-def train():
+# Defining train function
+def train(images_path: str, save_path: str):
 
+    # Defining dataset
     dataset = Folder(
         name='capsule',
-        root= r"C:\Users\Parker\Downloads\capsule\capsule\train",
+        root= images_path,
         normal_dir='good',
         task= TaskType.CLASSIFICATION
     )
 
     dataset.setup()
-    print(dataset)
 
+    # Creating model instance
     model = Padim()
+
+    # Creating engine instance
     engine = Engine(max_epochs = 50, 
                     task= TaskType.CLASSIFICATION)
 
-    
+    # Training the model
     engine.fit(datamodule=dataset, model=model)
 
+    # Saving the model
     engine.export(export_type=ExportType.TORCH,
                 model=model,
-                export_root=r'C:\Users\Parker\Downloads\capsule\weights')
-    
+                export_root=save_path)
+
+# Initiating training
 if __name__ == "__main__":
-    train()
+    train(images_path= path, save_path= path)
